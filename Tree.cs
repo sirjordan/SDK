565a5b82a2d87a83e3203bbe78ff3d178fd534f7
Martin <marinoff@yahoo.com>
2017-05-17 13:29:32 +0300

    refactored

--- Tree.cs
+++ Tree.cs
@@ -5,55 +5,6 @@ using System.Linq;
 
 public class Tree<T> : IEnumerable<T>
 {
-    public class TreeNode<T>
-    {
-        private IList<TreeNode<T>> childs;
-
-        public TreeNode(T value)
-        {
-            this.Value = value;
-            this.childs = new List<TreeNode<T>>();
-        }
-
-        public T Value { get; set; }
-
-        public IEnumerable<TreeNode<T>> Childs
-        {
-            get { return this.childs; }
-        }
-
-        public TreeNode<T> Parent { get; protected set; }
-
-        public void InsertChild(TreeNode<T> node)
-        {
-            this.childs.Add(node);
-            node.Parent = this;
-        }
-
-        public void Remove()
-        {
-            this.Parent.childs.Remove(this);
-        }
-
-        public override bool Equals(object obj)
-        {
-            TreeNode<T> other = obj as TreeNode<T>;
-            if (other == null)
-            {
-                return false;
-            }
-            else
-            {
-                return this.Value.Equals(other.Value);
-            }
-        }
-
-        public override int GetHashCode()
-        {
-            return this.Value.GetHashCode();
-        }
-    }
-
     public Tree(T root)
     {
         TreeNode<T> rootNode = new TreeNode<T>(root);
@@ -62,11 +13,14 @@ public class Tree<T> : IEnumerable<T>
 
     public TreeNode<T> Root { get; set; }
 
-    public IEnumerable<TreeNode<T>> NodeElements { get { return GetAllNodeElements(); } }
+    public IEnumerable<TreeNode<T>> NodeElements
+    {
+        get { return this.GetAllNodeElements(); }
+    }
 
     public void Insert(T elementToInsert, T parent)
     {
-        TreeNode<T> parentAsTreeNode = FindNode(this.Root, parent);
+        TreeNode<T> parentAsTreeNode = this.FindNode(this.Root, parent);
         if (parentAsTreeNode != null)
         {
             TreeNode<T> elementAsTreeNode = new TreeNode<T>(elementToInsert);
@@ -80,12 +34,13 @@ public class Tree<T> : IEnumerable<T>
 
     public void Remove(T elementToRemove)
     {
-        TreeNode<T> element = FindNode(this.Root, elementToRemove);
+        TreeNode<T> element = this.FindNode(this.Root, elementToRemove);
+
         if (element == null)
         {
             throw new InvalidOperationException(string.Format("Unable to find element: {0}", elementToRemove.ToString()));
-
         }
+
         if (element.Equals(this.Root))
         {
             throw new InvalidOperationException("Root cannot be deleted");
@@ -96,14 +51,25 @@ public class Tree<T> : IEnumerable<T>
 
     public IEnumerable<T> GetAllElements()
     {
-        IList<TreeNode<T>> childElements = GetAllNodeElements();
+        IList<TreeNode<T>> childElements = this.GetAllNodeElements();
         IEnumerable<T> elements = childElements.Select(n => n.Value);
         return elements;
     }
 
+    public IEnumerator<T> GetEnumerator()
+    {
+        IEnumerable<T> elements = this.GetAllElements();
+        return elements.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+
     protected IList<TreeNode<T>> GetAllNodeElements()
     {
-        IList<TreeNode<T>> elements = GetChilds(this.Root);
+        IList<TreeNode<T>> elements = this.GetChilds(this.Root);
         elements.Insert(0, this.Root);
         return elements;
     }
@@ -124,7 +90,7 @@ public class Tree<T> : IEnumerable<T>
             if (child != null)
             {
                 result.Add(child);
-                IList<TreeNode<T>> subChildren = GetChilds(child);
+                IList<TreeNode<T>> subChildren = this.GetChilds(child);
                 result.AddRange(subChildren);
             }
         }
@@ -135,18 +101,56 @@ public class Tree<T> : IEnumerable<T>
     protected TreeNode<T> FindNode(TreeNode<T> root, T searched)
     {
         // TODO: Implement DFS or BFS
-        IList<TreeNode<T>> elements = GetAllNodeElements();
+        IList<TreeNode<T>> elements = this.GetAllNodeElements();
         return elements.FirstOrDefault(x => x.Value.Equals(searched));
     }
 
-    public IEnumerator<T> GetEnumerator()
+    public class TreeNode<K>
     {
-        IEnumerable<T> elements = GetAllElements();
-        return elements.GetEnumerator();
-    }
+        private IList<TreeNode<K>> childs;
 
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
+        public TreeNode(K value)
+        {
+            this.Value = value;
+            this.childs = new List<TreeNode<K>>();
+        }
+
+        public K Value { get; set; }
+
+        public IEnumerable<TreeNode<K>> Childs
+        {
+            get { return this.childs; }
+        }
+
+        public TreeNode<K> Parent { get; protected set; }
+
+        public void InsertChild(TreeNode<K> node)
+        {
+            this.childs.Add(node);
+            node.Parent = this;
+        }
+
+        public void Remove()
+        {
+            this.Parent.childs.Remove(this);
+        }
+
+        public override bool Equals(object obj)
+        {
+            TreeNode<K> other = obj as TreeNode<K>;
+            if (other == null)
+            {
+                return false;
+            }
+            else
+            {
+                return this.Value.Equals(other.Value);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Value.GetHashCode();
+        }
     }
 }
